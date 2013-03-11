51e39a14cb720a25b4ba2714ad4db088666009d7
Jonathan Frazier <jjfraz11@gmail.com>
2013-03-10 21:04:36 -0400

    Create stockbot repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stockbot::Application.config.secret_token = '21250906e099806e13118d04b5aacf1a565612594ec71f2b11cc50d46c30ca09b9cfed8680c0df325e2b8a7348d33fe185e6a680d3a783471a7e7ea88ec6b746'
