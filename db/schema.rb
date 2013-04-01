4d3ccb8066305f4ca3dd53d1254ea10f7948d6ee
Jonathan Frazier <jjfraz11@gmail.com>
2013-04-01 12:24:20 -0400

    DB Schema update.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130331161154) do
+ActiveRecord::Schema.define(:version => 20130331171544) do
 
   create_table "sp500_stocks", :force => true do |t|
     t.string   "symbol"
