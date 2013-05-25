59eaf6b0637fe3b3830248ca57877a9ec469535e
Jonathan Frazier <jfraze@fraze-desktop.(none)>
2013-05-25 11:43:59 -0500

    Update bundle

--- db/migrate/20130331150430_rename_sp500_companies_to_sp500_stocks.rb
+++
@@ -1,9 +0,0 @@
-class RenameSp500CompaniesToSp500Stocks < ActiveRecord::Migration
-  def up
-    rename_table :sp500_companies, :sp500_stocks
-  end
-
-  def down
-    rename_table :sp500_stocks, :sp500_companies
-  end
-end
